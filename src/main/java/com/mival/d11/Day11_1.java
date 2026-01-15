65dc3259b68ebbe75eca251370b984a66f924e82
mival <mivalek@gmail.com>
2026-01-15 15:06:04 +0100

    simplified solution

--- src/main/java/com/mival/d11/Day11_1.java
+++ src/main/java/com/mival/d11/Day11_1.java
@@ -2,6 +2,7 @@ package com.mival.d11;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,36 +11,23 @@ import com.mival.utils.Util;
 
 
 public class Day11_1 {
-    static Map<String, Node> data;
+    static Map<String, String[]> data;
     static Map<String, Long> cache = new HashMap<>();
-    static class Node {
-        String label;
-        List<Node> children;
 
-        public Node(String label) {
-            this.label = label;
-            children = new ArrayList<>();
+    public static long dfs(String from, String parentLabel) {
+        if (cache.containsKey(from)) {
+            return cache.get(from);
         }
-
-        public void addChild(Node child) {
-            children.add(child);
-        }
-    }
-
-    public static long dfs(Node startNode, String parentLabel) {
-        String label = startNode.label;
-        if (cache.containsKey(label)) {
-            return cache.get(label);
-        }
-        if (label.equals("out")) {
+        if (from.equals("out")) {
                 cache.put(parentLabel, 1L);
                 return 1;
         }
-        long out = startNode.children
+        String children[] = data.get(from);
+        long out = Arrays.asList(children)
                     .stream()
-                    .map(x -> dfs(x, label))
+                    .map(x -> dfs(x, from))
                     .reduce(0L, Long::sum);
-        cache.put(label, out);
+        cache.put(from, out);
         return out;
     }
     
@@ -56,18 +44,11 @@ public class Day11_1 {
         data = new HashMap<>();
         for (String line: input) {
             String[] elems = line.split(": ");
-            String p = elems[0];
-            boolean isP = data.containsKey(p);
-            Node parent = isP ? data.get(p) : new Node(elems[0]);
-            if (!isP) data.put(elems[0], parent);
-            for (String c: elems[1].split(" ")) {
-                boolean isC = data.containsKey(c);
-                Node child = isC ? data.get(c) : new Node(c);
-                parent.addChild(child);
-                if (!isC) data.put(c, child);
-            }
+            String parent = elems[0];
+            String[] children = elems[1].split(" ");
+            data.put(parent, children);
         }
-        long res = dfs(data.get("you"), null);
+        long res = dfs("you", null);
         System.out.println(res);
     }
 }
--- src/main/java/com/mival/d11/Day11_2.java
+++ src/main/java/com/mival/d11/Day11_2.java
@@ -2,6 +2,7 @@ package com.mival.d11;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,37 +11,24 @@ import com.mival.utils.Util;
 
 
 public class Day11_2 {
-    static Map<String, Node> data;
+    static Map<String, String[]> data;
     static Map<String, Long> cache = new HashMap<>();
-    static class Node {
-        String label;
-        List<Node> children;
 
-        public Node(String label) {
-            this.label = label;
-            children = new ArrayList<>();
+    public static long dfs(String from, String to, String parentLabel, List<String> avoid) {
+        if (cache.containsKey(from)) {
+            return cache.get(from);
         }
-
-        public void addChild(Node child) {
-            children.add(child);
-        }
-    }
-
-    public static long dfs(Node from, String to, String parentLabel, List<String> avoid) {
-        String label = from.label;
-        if (cache.containsKey(label)) {
-            return cache.get(label);
-        }
-        if (avoid.contains(label)) return 0;
-        if (label.equals(to)) {
+        if (avoid.contains(from)) return 0;
+        if (from.equals(to)) {
                 cache.put(parentLabel, 1L);
                 return 1;
         }
-        long out = from.children
+        String children[] = data.get(from);
+        long out = Arrays.asList(children)
                     .stream()
-                    .map(x -> dfs(x, to, label, avoid))
+                    .map(x -> dfs(x, to, from, avoid))
                     .reduce(0L, Long::sum);
-        cache.put(label, out);
+        cache.put(from, out);
         return out;
     }
     
@@ -57,16 +45,9 @@ public class Day11_2 {
         data = new HashMap<>();
         for (String line: input) {
             String[] elems = line.split(": ");
-            String p = elems[0];
-            boolean isP = data.containsKey(p);
-            Node parent = isP ? data.get(p) : new Node(elems[0]);
-            if (!isP) data.put(elems[0], parent);
-            for (String c: elems[1].split(" ")) {
-                boolean isC = data.containsKey(c);
-                Node child = isC ? data.get(c) : new Node(c);
-                parent.addChild(child);
-                if (!isC) data.put(c, child);
-            }
+            String parent = elems[0];
+            String[] children = elems[1].split(" ");
+            data.put(parent, children);
         }
         long res = 0;
 
@@ -87,7 +68,7 @@ public class Day11_2 {
                 for (String a: new String[]{"fft", "dac", "out"}) {
                     if (a != sp[0] && a != sp[1]) avoid.add(a);
                 }
-                long result = dfs(data.get(sp[0]), sp[1], null, avoid);
+                long result = dfs(sp[0], sp[1], null, avoid);
                 subtotal *= result;
             }
             res += subtotal;
